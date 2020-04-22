0d3666a4ee17385d52e994818d629261f6f06ebc
unknown <kalebra1313@gmail.com>
2020-04-22 11:45:26 +0300

    save

---
+++ precache-manifest.a754a471e458221d4bec93934aa0b311.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c0d82ab7de120919db885aad3eff8c",
+    "url": "/gh-samuraj/index.html"
+  },
+  {
+    "revision": "ec2fe2000e7f225e6c09",
+    "url": "/gh-samuraj/static/css/3.4d89bd54.chunk.css"
+  },
+  {
+    "revision": "8c97e83d7a550d486ecb",
+    "url": "/gh-samuraj/static/css/4.f5fc9c19.chunk.css"
+  },
+  {
+    "revision": "25f56eafaa47e3cc83b9",
+    "url": "/gh-samuraj/static/css/main.e41bbfab.chunk.css"
+  },
+  {
+    "revision": "8d4f62b3e879674226a6",
+    "url": "/gh-samuraj/static/js/2.c8e6ccbe.chunk.js"
+  },
+  {
+    "revision": "ec2fe2000e7f225e6c09",
+    "url": "/gh-samuraj/static/js/3.17957a64.chunk.js"
+  },
+  {
+    "revision": "8c97e83d7a550d486ecb",
+    "url": "/gh-samuraj/static/js/4.95fc8a09.chunk.js"
+  },
+  {
+    "revision": "25f56eafaa47e3cc83b9",
+    "url": "/gh-samuraj/static/js/main.3a2d306e.chunk.js"
+  },
+  {
+    "revision": "7e34d2f078787557b719",
+    "url": "/gh-samuraj/static/js/runtime-main.bae563f7.js"
+  },
+  {
+    "revision": "421e15283a03b34ceeaa19fd2cc2350e",
+    "url": "/gh-samuraj/static/media/avatarMe.421e1528.png"
+  },
+  {
+    "revision": "a98424922149274b8a364358d02da9fe",
+    "url": "/gh-samuraj/static/media/oval.a9842492.svg"
+  }
+]);
